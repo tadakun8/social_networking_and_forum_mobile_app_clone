213eef4205e2995302dc0ccc1b8ea45117c76473
tadakun8 <tadakun8.lab@gmail.com>
2022-01-14 15:40:40 +0900

    feat: Add calender

--- lib/screens/schedule/schedule_screen.dart
+++ lib/screens/schedule/schedule_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:social_networking_and_forum_mobile_app_clone/constants.dart';
 import 'package:social_networking_and_forum_mobile_app_clone/screens/schedule/schedule_header.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class ScheduleScreen extends StatelessWidget {
   const ScheduleScreen({Key? key}) : super(key: key);
@@ -13,8 +14,41 @@ class ScheduleScreen extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(Constants.defaultPadding),
           child: Column(
-            children: const [
-              ScheduleHeader(),
+            children: [
+              const ScheduleHeader(),
+              Container(
+                padding: EdgeInsets.all(Constants.defaultPadding),
+                decoration: BoxDecoration(
+                  color: Constants.secondaryColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TableCalendar(
+                  daysOfWeekVisible: false,
+                  headerStyle: const HeaderStyle(
+                    titleCentered: true,
+                    formatButtonVisible: false,
+                    titleTextStyle:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  firstDay: DateTime(2020, 1, 1),
+                  lastDay: DateTime(2040, 12, 31),
+                  focusedDay: DateTime.now(),
+                  calendarBuilders: CalendarBuilders(
+                    todayBuilder: (context, date, events) => Container(
+                      margin: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Constants.textColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text(
+                        date.day.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
