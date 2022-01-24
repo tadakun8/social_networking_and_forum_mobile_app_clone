b045985e1f6eb226b7839416ed83cdb57f61e3d1
tadakun8 <tadakun8.lab@gmail.com>
2022-01-24 15:45:46 +0900

    fix: Fixed the background line to be scrollable as well.

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -18,126 +18,91 @@ class ProfileScreen extends StatelessWidget {
         // Wrap NoficationListener<OverscrollIndicatorNotification> to remove splash effect at last scroll
         body: Stack(
           children: [
-            Positioned(
-              top: -40,
-              left: -110,
-              child: Transform.rotate(
-                // rotate 45 degree
-                angle: pi / 4,
-                child: Container(
-                  width: 300,
-                  height: 300,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(45)),
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.orange.shade200,
-                          const Color(0xFFFBEFDA),
-                          Constants.primaryColor
-                        ],
-                        stops: const [
-                          0.1,
-                          0.3,
-                          0.5
-                        ]),
-                  ),
-                  child: Container(
-                    width: 297,
-                    height: 297,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(45)),
-                      color: Constants.primaryColor,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            // Positioned(
-            //   top: -120,
-            //   left: -100,
-            //   child: Transform.rotate(
-            //     // rotate 45 degree
-            //     angle: pi / 4,
-            //     child: Container(
-            //       decoration: BoxDecoration(
-            //         color: Colors.transparent,
-            //         borderRadius: BorderRadius.circular(40),
-            //         border: Border.all(
-            //           color: Colors.orange,
-            //         ),
-            //       ),
-            //       width: 300,
-            //       height: 300,
-            //     ),
-            //   ),
-            // ),
-            // Positioned(
-            //   top: -30,
-            //   left: -100,
-            //   child: Transform.rotate(
-            //     // rotate 45 degree
-            //     angle: pi / 4,
-            //     child: Container(
-            //       decoration: BoxDecoration(
-            //         color: Colors.transparent,
-            //         borderRadius: BorderRadius.circular(40),
-            //         border: Border.all(
-            //           color: Colors.orange,
-            //         ),
-            //       ),
-            //       width: 300,
-            //       height: 300,
-            //     ),
-            //   ),
-            // ),
-
             NotificationListener<OverscrollIndicatorNotification>(
               child: SingleChildScrollView(
-                child: Container(
-                  padding:
-                      EdgeInsets.symmetric(vertical: Constants.defaultPadding),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: Constants.defaultPadding),
-                        child: Column(
-                          children: [
-                            const ProfileHeader(),
-                            SizedBox(
-                              height: Constants.defaultPadding * 1.25,
-                            ),
-                            SizedBox(
-                              height: 250,
-                              child: ProfileInfo(),
-                            ),
-                            SizedBox(
-                              height: Constants.defaultPadding * 1.25,
-                            ),
-                            const Interest(),
-                          ],
+                  child: Stack(
+                children: [
+                  Positioned(
+                    top: -40,
+                    left: -110,
+                    child: Transform.rotate(
+                      // rotate 45 degree
+                      angle: pi / 4,
+                      child: Container(
+                        width: 300,
+                        height: 300,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(45)),
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Colors.orange.shade200,
+                                const Color(0xFFFBEFDA),
+                                Constants.primaryColor
+                              ],
+                              stops: const [
+                                0.1,
+                                0.3,
+                                0.5
+                              ]),
+                        ),
+                        child: Container(
+                          width: 297,
+                          height: 297,
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(45)),
+                            color: Constants.primaryColor,
+                          ),
                         ),
                       ),
-                      SizedBox(
-                        height: Constants.defaultPadding * 1.25,
-                      ),
-                      Schedule(),
-                      SizedBox(
-                        height: Constants.defaultPadding * 1.25,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: Constants.defaultPadding),
-                        child: const FloatingBottomBar(),
-                      )
-                    ],
+                    ),
                   ),
-                ),
-              ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: Constants.defaultPadding),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: Constants.defaultPadding),
+                          child: Column(
+                            children: [
+                              const ProfileHeader(),
+                              SizedBox(
+                                height: Constants.defaultPadding * 1.25,
+                              ),
+                              SizedBox(
+                                height: 250,
+                                child: ProfileInfo(),
+                              ),
+                              SizedBox(
+                                height: Constants.defaultPadding * 1.25,
+                              ),
+                              const Interest(),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: Constants.defaultPadding * 1.25,
+                        ),
+                        Schedule(),
+                        SizedBox(
+                          height: Constants.defaultPadding * 1.25,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: Constants.defaultPadding),
+                          child: const FloatingBottomBar(),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )),
               onNotification: (OverscrollIndicatorNotification notification) {
                 notification.disallowIndicator();
                 return false;
